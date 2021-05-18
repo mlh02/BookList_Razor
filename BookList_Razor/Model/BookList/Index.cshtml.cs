a63a7dee44078a3d6a6026bdf0034cc314e95812
Montather Handal <ali.louy2005@gmail.com>
2021-05-18 16:35:40 -0500

    Book Index OnGet Handler

---
+++ BookList_Razor/Model/BookList/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList_Razor.Model.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataBaseContext _db;
+
+        public IndexModel(DataBaseContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async void OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        }
+    }
+}
