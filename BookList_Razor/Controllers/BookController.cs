a05c84ab7fbcc340fff19f46cdf6c1113d5f2dc6
Montather Handal <ali.louy2005@gmail.com>
2021-05-19 11:56:28 -0500

    DataTable + Pageination + Delete Function

--- BookList_Razor/Controllers/BookController.cs
+++ BookList_Razor/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookList_Razor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,15 +16,24 @@ namespace BookListRazor.Controllers
     {
         private readonly DataBaseContext _db;
 
-        public BookController(DataBaseContext db)
-        {
-            _db = db;
-        }
-
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
             return Json(new { data = _db.Book.ToList() });
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { succues = true, message = "Delete successful" });
         }
     }
 }
